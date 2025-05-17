e93d20a2c35bdef3be524990df35951a69eaa1fa
PactForge <pactforge@gmail.com>
2025-05-17 19:31:34 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+```jsx
+  import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+  import Navbar from './components/Navbar';
+  import Sidebar from './components/Sidebar';
+  import Home from './pages/Home';
+  import Login from './pages/Login';
+  import Register from './pages/Register';
+  import PostDetail from './pages/PostDetail';
+  import './index.css';
+  import { useState } from 'react';
+
+  function App() {
+    const [darkMode, setDarkMode] = useState(false);
+
+    return (
+      <Router>
+        <div className={`min-h-screen ${darkMode ? 'dark bg-gray-900' : 'bg-gray-50'}`}>
+          <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+          <div className="flex">
+            <Sidebar />
+            <main className="flex-1 p-4 md:p-8">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/posts/:id" element={<PostDetail />} />
+              </Routes>
+            </main>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+
+  export default App;
+  ```
